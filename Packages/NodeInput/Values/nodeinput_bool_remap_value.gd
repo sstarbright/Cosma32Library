9212c0b61735fc3d603801f68953e0ed10f812e8
niaarchangel <explodingspears@gmail.com>
2024-09-15 15:25:00 -0230

    Initial commit

---
+++ Packages/NodeInput/Values/nodeinput_bool_remap_value.gd
@@ -0,0 +1,40 @@
+class_name InputRemapBool
+extends InputRemapValue
+
+signal on_down()
+signal on_up()
+
+var down = false
+var up = false
+
+func _ready():
+	var children = get_children()
+	for child in children:
+		if child is InputSource:
+			sources.append(child)
+			child.start(self)
+	current = false
+
+func _physics_process(delta):
+	._physics_process(delta)
+	down = false
+	up = false
+	for source in sources:
+		source.bool()
+		if (source.changed):
+			current = source.current
+			changed = true
+			down = source.down
+			up = source.up
+			
+			if down:
+				do_down()
+			elif up:
+				do_up()
+			break
+
+func do_down():
+	emit_signal("on_down")
+
+func do_up():
+	emit_signal("on_up")
