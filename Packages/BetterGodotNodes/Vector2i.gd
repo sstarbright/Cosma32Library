f1f8eeb79245aad38ff27c2b08620e8435c5bb48
niaarchangel <explodingspears@gmail.com>
2024-09-23 17:30:24 -0230

    fixed shadowing in Vector2i

--- Packages/BetterGodotNodes/Vector2i.gd
+++ Packages/BetterGodotNodes/Vector2i.gd
@@ -4,9 +4,9 @@ extends Reference
 var x: int
 var y: int
 
-func _init(x: int, y: int):
-	self.x = x
-	self.y = y
+func _init(initial_x: int, initial_y: int):
+	x = initial_x
+	y = initial_y
 
 func vector2() -> Vector2:
 	return Vector2(x, y)
