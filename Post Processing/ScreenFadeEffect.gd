8c0580955c16f06870309c35fead864358981459
niaarchangel <explodingspears@gmail.com>
2025-08-27 23:16:28 -0230

    added n64 postproc, improved fade

--- Post Processing/ScreenFadeEffect.gd
+++ Post Processing/ScreenFadeEffect.gd
@@ -17,7 +17,7 @@ func fade_in(fade_time : float, blend_mode : CanvasItemMaterial.BlendMode, fade_
 	if current_tween.is_valid():
 		current_tween.kill()
 	current_tween = get_tree().create_tween()
-	current_tween.tween_property(self, "color:a", 0.0, fade_time)
+	current_tween.tween_property(self, "color:a", 0.0, fade_time).set_trans(Tween.TRANS_CUBIC)
 	current_tween.tween_callback(Callable(self, "emit_signal").bind("fading_completed"))
 	current_tween.tween_callback(Callable(self, "hide"))
 
@@ -29,5 +29,5 @@ func fade_out(fade_time : float, blend_mode : CanvasItemMaterial.BlendMode, fade
 	if current_tween.is_valid():
 		current_tween.kill()
 	current_tween = get_tree().create_tween()
-	current_tween.tween_property(self, "color:a", 1.0, fade_time)
+	current_tween.tween_property(self, "color:a", 1.0, fade_time).set_trans(Tween.TRANS_CUBIC)
 	current_tween.tween_callback(Callable(self, "emit_signal").bind("fading_completed"))
